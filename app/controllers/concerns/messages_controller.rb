3ac0a78441c8df6c69ef49982f214174af076611
umair-rao <umairmuqeem@gmail.com>
2024-03-07 16:03:19 +0500

    Fix errors

--- app/controllers/concerns/messages_controller.rb
+++
@@ -1,5 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-    @greet = Message.random_greeting
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HelloWorld
+module HelloRailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
@@ -16,7 +14,7 @@ module HelloWorld
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -25,5 +23,10 @@ module HelloWorld
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
